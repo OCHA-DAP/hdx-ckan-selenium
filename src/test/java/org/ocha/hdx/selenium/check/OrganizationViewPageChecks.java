9128146c9a7af61259d93150a77e57857e2ca364
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2014-09-26 20:04:23 +0300

    Adding checks for add member - checking that the search string itself is not shown in search-as-you-type - checking that a nice error message appears if the user clicks submit with an empty username

--- src/test/java/org/ocha/hdx/selenium/check/OrganizationViewPageChecks.java
+++ src/test/java/org/ocha/hdx/selenium/check/OrganizationViewPageChecks.java
@@ -5,15 +5,22 @@ package org.ocha.hdx.selenium.check;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.ocha.hdx.selenium.util.Util.FF;
 import static org.ocha.hdx.selenium.util.Util.REMOVE;
 import static org.ocha.hdx.selenium.util.Util.WD;
 
+import java.util.Map;
+
 import org.ocha.hdx.selenium.util.Config;
 import org.ocha.hdx.selenium.util.ContextConstants;
+import org.ocha.hdx.selenium.util.GenericFind;
 import org.ocha.hdx.selenium.util.SelectorConstants;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * @author alexandru-m-g
@@ -46,6 +53,43 @@ public class OrganizationViewPageChecks {
 	public static ICheckAction userFromConfigInMemberListAsEditorCheck = context -> {
 		context.put(ContextConstants.ROLE, "editor");
 		userFromConfigInMemberListCheck.doAction(context);
+	};
+	
+	public static ICheckAction wrongUsernameCheck = new ICheckAction() {
+		
+		@Override
+		public void doAction(Map<String, Object> context) {
+			String username = "xz12-non-existing-username";
+			WD(context).findElement(By.partialLinkText("ADD MEMBER")).click();
+
+			new WebDriverWait(WD(context),5).until( 
+					(ExpectedCondition<Boolean>) d -> d.findElement(By.cssSelector("#s2id_username a"))!=null
+					);
+			WD(context).findElement(By.cssSelector("#s2id_username a")).click();
+			WebElement usernameInputBox = FF(context, GenericFind.class).byCSSSelectorAndDisplayed("input.select2-input"); 
+			usernameInputBox.sendKeys(username);
+			new WebDriverWait(WD(context),5).until( 
+					(ExpectedCondition<Boolean>) d -> !usernameInputBox.getAttribute("class").contains("select2-active")
+					);
+			
+			try {
+				WebElement noResult = WD(context).findElement(By.cssSelector(".select2-no-results"));
+				assertNotNull(noResult);
+			}
+			catch (NoSuchElementException e) {
+				fail("The 'no-result' message should appear as the search-as-you-type result");
+			}
+			
+			WD(context).findElement(By.cssSelector(".dataset-form .hdx-submit-btn")).click();
+			
+			try {
+				WebElement errorMsg = WD(context).findElement(By.cssSelector("div.flash-messages div.alert"));
+				assertNotNull(errorMsg);
+			}
+			catch (NoSuchElementException e) {
+				fail("An error message should be displayed when the user clicks submit without writing a username");
+			}
+		}
 	};
 
 }
